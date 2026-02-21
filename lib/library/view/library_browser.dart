2fc3609b275390968371d7429ed703ee704725e2
y-ploni <7353755@gmail.com>
2026-02-21 21:38:39 +0200

    Revert "מחיקת ספרים"
    This reverts commit 377ce852a22e699533af38fd74029948a7e6739c.

--- lib/library/view/library_browser.dart
+++ lib/library/view/library_browser.dart
@@ -655,10 +655,6 @@ class _LibraryBrowserState extends State<LibraryBrowser>
                   _openBookInReader(book, index);
                 }
               },
-              onBookDeleted: () {
-                // רענון הספרייה לאחר מחיקת ספר
-                context.read<LibraryBloc>().add(RefreshLibrary());
-              },
             ),
           ),
         );
